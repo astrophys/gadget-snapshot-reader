752b942db63342afd4459ba0123070f82bc0e32f
Ali Snedden <Ali.Snedden@nationwidechildrens.org>
2022-06-07 21:36:41 -0400

    WHERE_I_LEFT_OFF.txt : 1. Removed task
    src/snapshot-reader.py :
    1. Added 2nd [option] argument to CL args

--- src/snapshot-reader.py
+++ src/snapshot-reader.py
@@ -38,8 +38,9 @@ def print_help(Arg):
     FUTURE:
     """
     sys.stdout.write(
-            "\nUSAGE : python3 snapshot-reader.py snapshot_XXX\n\n"
+            "\nUSAGE : python3 snapshot-reader.py snapshot_XXX [option]\n\n"
             "       snapshot_XXX : (str) path to a Gadget snapshot file\n"
+            "       [option]     : (str) [plot|out_cmp_gas|out_cmp_star|out_gas_matlab]\n"
             "                         \n")
     sys.exit(Arg)
 
@@ -76,12 +77,13 @@ def main():
                     sys.version_info[0]))
 
     ######### Get Command Line Options ##########
-    if(len(sys.argv) != 2):
+    if(len(sys.argv) != 2 and len(sys.argv) != 3):
         print_help(1)
     elif(len(sys.argv) == 2 and (sys.argv[1] == "--help" or sys.argv[1] == "-help")):
         print_help(0)
     else:
         path=sys.argv[1]
+        option=sys.argv[2]
     # Endianness matters
     endian = "little"
     if(endian == "little"):
@@ -96,62 +98,64 @@ def main():
     (pL, header) = read_gadget2_snapshot(Path=path, Endian=edn)
 
     ## Temporarily comment out for expediency
-    ### ###### Let's try to plot the mass density ######
-    ### array = np.zeros([100,100],dtype=np.float64)
-    ### # gas, dm and stars 
-    ### for k in range(len(pL)):
-    ###     # Check logic here
-    ###     i = int(pL[k].posV[0]/header.boxSize * array.shape[0])
-    ###     j = int(pL[k].posV[1]/header.boxSize * array.shape[1])
-    ###     #print(i,j)
-    ###     array[i,j] += pL[k].mass
-    ### 
-    ### array = array / (header.npartV[0] + header.npartV[1] + header.npartV[2] +
-    ###                  header.npartV[3] + header.npartV[4] + header.npartV[5])
-    ### array = np.log(array)
-    ### fig, ax = plt.subplots()
-    ### im = ax.imshow(array)
-    ### plt.show()
-    ### 
-    ### ###### Let's try to plot the Carbon mass Frac######
-    ### array = np.zeros([100,100],dtype=np.float64)
-    ### # gas, dm and stars 
-    ### for k in range(len(pL)):
-    ###     if(pL[k].type == 0 or pL[k].type ==4):
-    ###         # Check logic here
-    ###         i = int(pL[k].posV[0]/header.boxSize * array.shape[0])
-    ###         j = int(pL[k].posV[1]/header.boxSize * array.shape[1])
-    ###         #print(i,j)
-    ###         array[i,j] += pL[k].metalMassD['C']
-    ### 
-    ### array = array / (header.npartV[0] + header.npartV[1] + header.npartV[2] +
-    ###                  header.npartV[3] + header.npartV[4] + header.npartV[5])
-    ### array = np.log(array)
-    ### fig, ax = plt.subplots()
-    ### im = ax.imshow(array)
-    ### plt.show()
-    ###   
+    ###### Let's try to plot the mass density ######
+    if(option == plot):
+        array = np.zeros([100,100],dtype=np.float64)
+        # gas, dm and stars 
+        for k in range(len(pL)):
+            # Check logic here
+            i = int(pL[k].posV[0]/header.boxSize * array.shape[0])
+            j = int(pL[k].posV[1]/header.boxSize * array.shape[1])
+            #print(i,j)
+            array[i,j] += pL[k].mass
+        
+        array = array / (header.npartV[0] + header.npartV[4])
+        array = np.log(array)
+        fig, ax = plt.subplots()
+        im = ax.imshow(array)
+        plt.show()
+        
+        ###### Let's try to plot the Carbon mass Frac######
+        array = np.zeros([100,100],dtype=np.float64)
+        # gas, dm and stars 
+        for k in range(len(pL)):
+            if(pL[k].type == 0 or pL[k].type ==4):
+                # Check logic here
+                i = int(pL[k].posV[0]/header.boxSize * array.shape[0])
+                j = int(pL[k].posV[1]/header.boxSize * array.shape[1])
+                #print(i,j)
+                array[i,j] += pL[k].metalMassD['C']
+        
+        array = array / (header.npartV[0] + header.npartV[4])
+        array = np.log(array)
+        fig, ax = plt.subplots()
+        im = ax.imshow(array)
+        plt.show()
+      
+    elif(option == "out_cmp_gas"):
+        ###### Let's try to Replicate gadget2/128part-25mpc/snapshot_016/mnc2file_gas.txt ######
+        print("Writing gas particles to output_gas.txt")
+        fout = open("output_gas.txt", "w+")
+        for p in pL:
+            if(p.type == 0):
+                fout.write("{:<.6e} {:<.6e} {:<.6e} {:<.6f} {:<.6f} {:<.6f} {:<.6f} {:<.6f} "
+                           "{:<.6f}\n".format(p.rho, p.u, p.mass, p.metallicityD['C'],
+                           p.metallicityD['O'], p.metallicityD['Ca'], p.metallicityD['Cr'],
+                           p.metallicityD['Mn'], p.metallicityD['Fe']))
 
-    ### ###### Let's try to Replicate gadget2/128part-25mpc/snapshot_016/mnc2file_gas.txt ######
-    ### print("Writing gas particles to output_gas.txt")
-    ### fout = open("output_gas.txt", "w+")
-    ### for p in pL:
-    ###     if(p.type == 0):
-    ###         fout.write("{:<.6e} {:<.6e} {:<.6e} {:<.6f} {:<.6f} {:<.6f} {:<.6f} {:<.6f} "
-    ###                    "{:<.6f}\n".format(p.rho, p.u, p.mass, p.metallicityD['C'],
-    ###                    p.metallicityD['O'], p.metallicityD['Ca'], p.metallicityD['Cr'],
-    ###                    p.metallicityD['Mn'], p.metallicityD['Fe']))
+    elif(option == "out_cmp_star"):
+        ##### Let's try to Replicate gadget2/128part-25mpc/snapshot_016/mnc2file_star.txt ######
+        print("Writing gas particles to output_star.txt")
+        fout = open("output_star.txt", "w+")
+        for p in pL:
+            if(p.type == 4):
+                fout.write("{:<.6e} {:<.6e} {:<.6f} {:<.6f} {:<.6f} {:<.6f} {:<.6f} "
+                           "{:<.6f}\n".format(p.mass, p.age, p.metallicityD['C'],
+                           p.metallicityD['O'], p.metallicityD['Ca'], p.metallicityD['Cr'],
+                           p.metallicityD['Mn'], p.metallicityD['Fe']))
+    elif(option == "out_gas_off"):
 
-    ###### Let's try to Replicate gadget2/128part-25mpc/snapshot_016/mnc2file_star.txt ######
-    print("Writing gas particles to output_star.txt")
-    fout = open("output_star.txt", "w+")
-    for p in pL:
-        if(p.type == 4):
-            fout.write("{:<.6e} {:<.6e} {:<.6f} {:<.6f} {:<.6f} {:<.6f} {:<.6f} "
-                       "{:<.6f}\n".format(p.mass, p.age, p.metallicityD['C'],
-                       p.metallicityD['O'], p.metallicityD['Ca'], p.metallicityD['Cr'],
-                       p.metallicityD['Mn'], p.metallicityD['Fe']))
-    
+        
     
     print("\n\nEnded : %s"%(time.strftime("%D:%H:%M:%S")))
     print("Run Time : {:.4f} h".format((time.time() - startTime)/3600.0))
